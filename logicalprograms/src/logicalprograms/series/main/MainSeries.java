7ad1e0e18ccb6c8d8bd946c5f790a9198baab13a
Admin <Admin@DESKTOP-NGTLNH4>
2025-01-13 16:47:26 +0530

    Fibonnacies Series

---
+++ logicalprograms/src/logicalprograms/series/main/MainSeries.java
@@ -0,0 +1,14 @@
+package logicalprograms.series.main;
+
+import logicalprograms.series.Series;
+
+public class MainSeries {
+	public static void main(String[] args) {
+		Series series=new Series();
+		
+		series.setLimit(10);
+		//series.printFibonnaciSeries();
+		series.printCubeSquareSeries();
+	}
+	}
+
